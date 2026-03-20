a24659c1cd00f04e1225abe13d1a6001929a0d2e
MarsianCreator <scrambledstrawberry@gmail.com>
2026-03-20 18:11:01 +0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
+	// 替换为你的 GitHub Pages 地址
+	// 格式：https://<你的用户名>.github.io/<项目仓库名>/
+	site: 'https://MarsianCreator.github.io/INGRESS-GUIDE/', 
+	base: '/INGRESS-GUIDE', // 必须和仓库名一致，且以斜杠开头
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'INGRESS指南',
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/MarsianCreator/INGRESS-GUIDE' },
+			],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+				{ label: '开始使用', link: '/introduction/' },
+				{ label: '进阶教程', autogenerate: { directory: 'guides' } },
 			],
 		}),
 	],
